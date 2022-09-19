f992c5fa015f30ec86e1e124e0979279df1f5f9a
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-18 23:07:50 -0300

    PROXIMA AULA E ANIMAL REGEX PHORA

---
+++ src/application/Program_03.java
@@ -0,0 +1,31 @@
+package application;
+
+/**
+ * FIXME OLHA TANTO QUE SOU VELHO EM JAVA -> UNIVERSIDADE XTI FIXME O CARA
+ * POSTOU ESSAS VÍDEO AULAS EM 2011
+ * @author LUCAS ROCHA -> VOLTANDO A ATIVA
+ * @since DESDE DE 2014
+ * @version 1.0
+ */
+public class Program_03 {
+	
+	/**@param stringBuffer
+	 * @param stringBuilder
+	 * 
+	 * */
+	
+	public static StringBuffer stringBuffer = new StringBuffer();
+	public static StringBuilder stringBuilder = new StringBuilder();
+
+	public static void main(String[] args) {
+	  
+		
+		stringBuffer.append("LUCAS ROCHA DOS SANTOS");
+	    stringBuilder.append("ANNA CLARA BERNARDINO");
+	    
+	    System.out.println(stringBuffer.capacity());
+	    System.out.println(stringBuffer.reverse());
+	    
+	}
+	
+}
